5ea9ad5ea70eab4367289d5896ed906f1bcd320f
Edilson Silva <eddysilva1994@gmail.com>
2025-08-08 00:16:20 -0300

    feat: set db delete cascade in users relationship with bank accounts

---
+++ prisma/migrations/20250808031447_set_delete_cascade_in_users_relationship_with_bank_accounts/migration.sql
@@ -0,0 +1,5 @@
+-- DropForeignKey
+ALTER TABLE "public"."bank_accounts" DROP CONSTRAINT "bank_accounts_user_id_fkey";
+
+-- AddForeignKey
+ALTER TABLE "public"."bank_accounts" ADD CONSTRAINT "bank_accounts_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "public"."users"("id") ON DELETE CASCADE ON UPDATE CASCADE;
