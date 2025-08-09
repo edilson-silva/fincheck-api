6319de5a85317ed96a9b93207a09320e98d2ab9e
Edilson Silva <eddysilva1994@gmail.com>
2025-08-09 14:19:58 -0300

    feat(user): check if email is already in use on user create

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { ConflictException, Injectable } from '@nestjs/common';
 import { PrismaService } from 'src/database/prisma.service';
 import { CreateUserDto } from './dto/create-user.dto';
 
@@ -7,12 +7,18 @@ export class UsersService {
   constructor(private readonly prismaClient: PrismaService) {}
 
   async create(createUserDto: CreateUserDto) {
+    const { name, email, password } = createUserDto;
+
+    const emailAlreadyInUse = await this.prismaClient.user.findUnique({
+      where: { email },
+    });
+
+    if (emailAlreadyInUse) {
+      throw new ConflictException('E-mail already in use');
+    }
+
     const user = this.prismaClient.user.create({
-      data: {
-        name: createUserDto.name,
-        email: createUserDto.email,
-        password: createUserDto.password,
-      },
+      data: { name, email, password },
     });
 
     return user;
