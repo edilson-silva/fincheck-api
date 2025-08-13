4a810d75ea08feefb5403ff1e896a5ffab830672
Edilson Silva <eddysilva1994@gmail.com>
2025-08-12 23:36:52 -0300

    feat(auth): add a custom decorator to allow public routes

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -1,8 +1,10 @@
 import { Body, Controller, Post } from '@nestjs/common';
+import { IsPublic } from 'src/shared/decorators/public.decorator';
 import { AuthService } from './auth.service';
 import { SigninInputDto, SigninOutputDto } from './dtos/signin';
 import { SignupInputDto, SignupOutputDto } from './dtos/signup';
 
+@IsPublic()
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
--- src/modules/auth/auth.guard.ts
+++ src/modules/auth/auth.guard.ts
@@ -4,16 +4,28 @@ import {
   Injectable,
   UnauthorizedException,
 } from '@nestjs/common';
+import { Reflector } from '@nestjs/core';
 import { JwtService } from '@nestjs/jwt';
 import { Request } from 'express';
 import { env } from 'src/shared/config/env';
+import { IS_PUBLIC } from 'src/shared/decorators/public.decorator';
 
 @Injectable()
 export class AuthGuard implements CanActivate {
-  constructor(private jwtService: JwtService) {}
+  constructor(
+    private readonly jwtService: JwtService,
+    private readonly reflector: Reflector,
+  ) {}
 
   async canActivate(context: ExecutionContext): Promise<boolean> {
-    const request = context.switchToHttp().getRequest();
+    const isPublic = this.reflector.getAllAndOverride<boolean>(IS_PUBLIC, [
+      context.getHandler(),
+      context.getClass(),
+    ]);
+
+    if (isPublic) return true;
+
+    const request: Request = context.switchToHttp().getRequest();
     const token = this.extractTokenFromHeaders(request);
 
     if (!token) {
